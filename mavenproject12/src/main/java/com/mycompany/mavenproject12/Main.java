b85c29f2b88b58d0e84b641a3d33220637ff1fd3
LeonardoHR26 <93102919+LeonardoHR26@users.noreply.github.com>
2021-11-28 04:38:36 -0400

    Add files via upload

---
+++ mavenproject12/src/main/java/com/mycompany/mavenproject12/Main.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.mavenproject12;
+public class Main {
+    public static void main(String []args){
+       UwU P1= new UwU();
+       
+           
+       }
+        
+        
+        
+        
+        
+    }
+        
+   
